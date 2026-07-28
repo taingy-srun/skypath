ddf804978f108e985952eaa3eb6fdc2f23dfaa8b
Taingy Srun <taingy.srun20@gmail.com>
2026-07-28 11:12:32 -0700

    Init backend project

---
+++ backend/src/test/java/com/skypath/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypath.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
